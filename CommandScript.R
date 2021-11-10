c3e9b6caf5b7dca57131219247d7bec406c86db1
JoranTiU <88381244+JoranTiU@users.noreply.github.com>
2021-11-10 16:51:41 +0100

    Layout update

--- CommandScript.R
+++ CommandScript.R
@@ -12,8 +12,16 @@ blogdown::check_hugo()
 blogdown::config_Rprofile()
 
 blogdown::new_post(
-  title = "STAN Code for Analyzing Intensive Longitudinal Data: Part IIa - Hierarchical Autogeressive Models",
+  title = "STAN Code for Analyzing Intensive Longitudinal Data: Part IIa - Hierarchical Autoregressive Models",
   date = '2021-11-02',
   slug = 'STAN-Hierarchical-AR',
+  tags = c("Bayesian", "STAN", "AR", "ILD", "R","tutorial")
+)
+
+
+blogdown::new_post(
+  title = "STAN Code for Analyzing Intensive Longitudinal Data: Part IIb - Hierarchical Autoregressive Models with Missing Data",
+  date = '2021-11-09',
+  slug = 'STAN-Hierarchical-AR-Miss',
   tags = c("Bayesian", "STAN", "AR", "ILD", "R","tutorial")
 )
